d0b37ca57a335cf23b59d56f8e94a2f0387e6846
svh03ra <shallowgust170@gmail.com>
2026-06-04 06:24:44 +0000

    1.0 Release!

--- src/modes/states/graphical/login/ui/login.c
+++ src/modes/states/graphical/login/ui/login.c
@@ -27,6 +27,6 @@ static void render_login(void) {
     draw_text(text_x, 227, line3, color_black, 0, true);
     draw_text(505, 10, "Version 1.0:", color_black, color_gray_light, true);
     draw_text(505, 20, HALOXOS_BUILD_TEXT, color_black, color_gray_light, true);
-    draw_text_center(OS_WIDTH / 2, 460, "(C) 2026 Svh03ra", color_black, color_gray_light, true);
+    draw_text_center(OS_WIDTH / 2, 460, "(C) 2026 Svh03ra, Final Release.", color_black, color_gray_light, true);
     draw_text_center(OS_WIDTH / 2, 450, "Available here! https://github.com/svh03ra/HaloxOS", color_black, color_gray_light, true);
 }
